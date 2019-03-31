2e9ee57cae98bb6fc08d48456f0d06dbc09e327d
TaeGeun Moon <ahn_v3@naver.com>
2019-03-31 12:48:54 +0900

    fix to work as daemon

--- index.js
+++ index.js
@@ -1,12 +1,55 @@
 const MongoClient = require('mongodb').MongoClient;
 const request = require('request');
 const util = require('util');
+const mail = require('./mail');
 require('dotenv').config();
 const client = new MongoClient(process.env.MONGODB_URI, {
   useNewUrlParser: true
 });
 
-const options = {
+async function getLastId(type) {
+  const cursor = client
+    .db('skku')
+    .collection(type)
+    .find()
+    .sort({
+      _id: -1
+    })
+    .limit(1);
+
+  const toArray = util.promisify(cursor.toArray.bind(cursor));
+  return toArray()
+    .then(docs => {
+      return docs[0]['_id'];
+    });
+}
+
+function fetchNewNotices(params) {
+  const [type, typeKor, recentId, res] = params
+  let data = JSON.parse(res.body).aaData;
+  data.forEach(element => {
+    element['_id'] = element.id;
+    delete element.id;
+  });
+  data = data.filter(notice => {
+    return notice._id > recentId;
+  });
+  console.log('Fetching data ID after : ' + recentId);
+  console.log(`${data.length} fetched`);
+
+  if (data.length > 0) {
+    const collection = client.db('skku').collection(type);
+    return collection.insertMany(data).then(result => {
+      if (result.result.ok == 1 && result.result.n > 0) {
+        mail.sendMail(`[SKKU] ${data.length} 개의 새로운 ${typeKor}`, `<h1>${typeKor}</h1>` + data.map(notice => {
+          return `[${notice._id}] (${notice.category}) ${notice.title}\t\t${notice.time}`
+        }).join('<br/><br/>>'));
+      }
+    });
+  }
+}
+
+const options_notice = {
   url: process.env.REST_NOTICE,
   headers: {
     'User-Agent': 'node.js',
@@ -14,91 +57,23 @@ const options = {
     Host: 'cs.skku.edu'
   }
 };
+const options_recruit = {
+  url: process.env.REST_RECRUIT,
+  headers: {
+    'User-Agent': 'node.js',
+    Accept: '*/*',
+    Host: 'cs.skku.edu'
+  }
+};
 
-async function getLastId() {
-  const connect = util.promisify(client.connect.bind(client));
-  return connect()
-    .then(err => {
-      const cursor = client
-        .db('skku')
-        .collection('recruit')
-        .find()
-        .sort({
-          _id: -1
-        })
-        .limit(1);
-      const toArray = util.promisify(cursor.toArray.bind(cursor));
-      return toArray();
-    })
-    .then(docs => {
-      return docs[0]['_id'];
-    });
+function checkNews() {
+  const requestAsync1 = util.promisify(request);
+  const requestAsync2 = util.promisify(request);
+  Promise.all(['notice', '공지사항', getLastId('notice'), requestAsync1(options_notice)]).then(fetchNewNotices);
+  Promise.all(['recruit', '취업 정보', getLastId('recruit'), requestAsync2(options_recruit)]).then(fetchNewNotices);
 }
 
-getLastId().then(id => {
-  console.log(id);
-});
 
-request(options, (err, res, body) => {
-  if (err) {
-    console.log('err');
-  }
-  data = JSON.parse(body).aaData;
-  data.forEach(element => {
-    element['_id'] = element.id;
-    delete element.id;
-  });
-
-  client.connect(err => {
-    const collection = client.db('skku').collection('recruit');
-    collection.insertMany(data).then(result => {
-      console.log(result);
-    });
-    console.log(err);
-    // perform actions on the collection object
-    client.close();
-  });
-});
-
-/* client.connect((err) => {
-  console.log(client.db('skku').collection('recruit').find().limit(1).toArray((err, docs) => {
-    console.log(docs);
-  }));
-}) */
-/* 
-let data;
-
-request(options, (err, res, body) => {
-  if (err) {
-    console.log('err');
-  }
-  data = JSON.parse(body).aaData;
-  data.forEach(element => {
-    element['_id'] = element.id;
-    delete element.id;
-  });
-
-  client.connect(err => {
-    const collection = client.db('skku').collection('recruit');
-    collection.insertMany(data).then(result => {
-      console.log(result);
-    });
-    console.log(err);
-    // perform actions on the collection object
-    client.close();
-  });
-}); */
-
-/*
-curl 'https://cs.skku.edu/rest/board/list/notice'\ -
-    XGET\ -
-    H 'Cookie: connect.sid=s%3AdZyoGveCmZwS9_F_aLE4AAJPOkmkbl1I.5apvdsQUyUHZ2IXRIUgTar8csjtkB6LLk8y7aVYrTqY; _ga=GA1.2.836153827.1552826266; loginMemberId=ahnv3'\ -
-    H 'Accept-Language: ko-kr'\ -
-    H 'Host: cs.skku.edu'\ -
-    H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.0.3 Safari/605.1.15'\ -
-    H 'Referer: https://cs.skku.edu/news/notice/list'\ -
-    H 'Accept-Encoding: br, gzip, deflate'\ -
-    H 'Connection: keep-alive'\ -
-    H 'X-Requested-With: XMLHttpRequest'
-
-    */
+client.connect(err => {
+  setInterval(checkNews, 30 * 60 * 1000);
+})
